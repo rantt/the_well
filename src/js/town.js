de2cacb4260103c9b97df4c51eaf968d348d4327
Dave Canam <david.canam@gmail.com>
2014-11-18 13:42:47 -0400

    jslint cleanup of town.js

--- src/js/town.js
+++ src/js/town.js
@@ -1,6 +1,9 @@
 /*global Game*/
 /*global player*/
 /*global Npc*/
+/*global tileSize*/
+/*global dialogue*/
+/*global spaceKey*/
 
 // var musicOn = true;
 
@@ -63,20 +66,20 @@ Game.Town.prototype = {
     this.npcs = this.game.add.group();
 
     this.lines = {
-                  'mom':  {1: '*Hi honey.*Beautiful day, isn\'t it?',
+                  mom:  {1: '*Hi honey.*Beautiful day, isn\'t it?',
                            2: '*You wanna play with Jack?*Oh, ok.  Have fun.*Be home for dinner.',
                            3: '*There might be some in the house.*Why don\'t you ask your father.',
                            4: '*There might be some in the house.*Why don\'t you ask your father.'},
-                  'jack': {1: '*Hey, wanna play?*Let\'s go to the old well.*Better ask your mom first.',
+                  jack: {1: '*Hey, wanna play?*Let\'s go to the old well.*Better ask your mom first.',
                            2: '*We should go down there.*We\'ll need some stuff though.*Go get some rope and a light.',
                            3: '*We should go down there?*We\'ll need some stuff though.*Go get some rope and a light.',
                            4: '*Oh, good you got everything!*What are we waiting for?, Let\'s go!',
-                           7: '*Well, I guess this is it.'},
-                  'clara': {1: '*Hi, nice day for skipping!',
+                           7: '*I guess you needed this.*See you around...'},
+                  clara: {1: '*Hi, nice day for skipping!',
                             2: '*Hey, wanna play?*Jack huh? You always play with him.*Nevermind then!',
                             3: '*Rope?*I have skipping rope. Will that work?',
                             4: '*Rope?*I have skipping rope. Will that work?'}
-                } 
+                }; 
 
 
     this.physics.p2.convertTilemap(this.map, this.layer1);
@@ -91,16 +94,16 @@ Game.Town.prototype = {
     
     //Add Jack
     if ((this.scene === 1) || (this.scene === 7)) {
-      this.jack = new Npc(this.game,tileSize*10, tileSize*7,'jack', 9, this.lines['jack'][this.scene]);
+      this.jack = new Npc(this.game,tileSize*10, tileSize*7,'jack', 9, this.lines.jack[this.scene]);
     }else {
-      this.jack = new Npc(this.game,tileSize*5, tileSize*15,'jack', 9, this.lines['jack'][this.scene]);
+      this.jack = new Npc(this.game,tileSize*5, tileSize*15,'jack', 9, this.lines.jack[this.scene]);
     }
 
     if (this.scene !== 7 ) {
-      this.npcs.add(new Npc(this.game,tileSize*9, tileSize*3,'mom', 0, this.lines['mom'][this.scene] )); 
+      this.npcs.add(new Npc(this.game,tileSize*9, tileSize*3,'mom', 0, this.lines.mom[this.scene] )); 
 
       //Add Clara
-      this.clara = new Npc(this.game,tileSize*16, tileSize*6,'clara', 0, this.lines['clara'][this.scene]);
+      this.clara = new Npc(this.game,tileSize*16, tileSize*6,'clara', 0, this.lines.clara[this.scene]);
       this.clara.animations.add('skipping',[12,13,14],6,true);        
       this.clara.animations.play('skipping');
 
@@ -116,27 +119,25 @@ Game.Town.prototype = {
     this.map.createFromObjects('objects', 30, 'town', 29, true, false, this.exitPoints);
     this.map.createFromObjects('objects', 34, 'town', 33, true, false, this.exitPoints);
 
-
     // Initial Player Position by tile
-    if (Game.lastLocation == "MyHouse") {
-      Game.camera = {x:0, y:0}
+    if (Game.lastLocation === 'MyHouse') {
+      Game.camera = {x:0, y:0};
       player.tilex = 5;
       player.tiley = 6;
-      Game.lastLocation = "Town";
-    }else if (Game.lastLocation == "Gramps") {
+      Game.lastLocation = 'Town';
+    }else if (Game.lastLocation === 'Gramps') {
       //Move to house without tweening
-      Game.camera = {x:1, y:1}
+      Game.camera = {x:1, y:1};
       this.game.camera.x = Game.camera.x*Game.w;
       this.game.camera.y = Game.camera.y*Game.h;
 
       player.tilex = 20;
       player.tiley = 17;
-      Game.lastLocation = "Town";
+      Game.lastLocation = 'Town';
     }
 
     player.create();
     dialogue.create();
-
 
   },
   updateCharacterLines: function() {
@@ -172,21 +173,6 @@ Game.Town.prototype = {
   },
   update: function() {
 
-    if (this.scene !== 7) {
-      if (dialogue.speaker === this.clara) {
-        this.clara.animations.stop();
-      }else {
-        if (this.clara.animations.currentAnim != 'skipping') {
-          this.clara.play('skipping');
-        }
-      }
-    }else{
-      if ((this.jack.spoke === true) && (!dialogue.typing) && (dialogue.hidden)) {
-        this.jackLeaves(7);
-      }
-    }
-
-
     //Scene 1 Ends When Jack Leaves
     if ((this.jack.spoke === true) && (!dialogue.typing) && (dialogue.hidden) && (this.scene === 1)) {
      this.jackLeaves(2); 
@@ -199,25 +185,40 @@ Game.Town.prototype = {
       this.updateCharacterLines();
     }
 
+
+    //Scene 7 End Sequence
+    if (this.scene !== 7) {
+      if (dialogue.speaker === this.clara) {
+        this.clara.animations.stop();
+      }else {
+        if (this.clara.animations.currentAnim !== 'skipping') {
+          this.clara.play('skipping');
+        }
+      }
+    }else{
+      if ((this.jack.spoke === true) && (!dialogue.typing) && (dialogue.hidden)) {
+        this.jackLeaves(7);
+      }
+    }
+
+    //Trigger Exit points
     this.exitPoints.forEach(function(ep) {
-      b1 = ep.getBounds();
-      bp = player.sprite.getBounds();
+      var b1 = ep.getBounds();
+      var bp = player.sprite.getBounds();
       if (Phaser.Rectangle.intersects(b1,bp)) {
         console.log(ep.destination);
-        if ((ep.destination != 'Well') ||  ((ep.destination === 'Well') && (this.haveLamp === true) && (this.haveRope === true))) {
+        if ((ep.destination !== 'Well') ||  ((ep.destination === 'Well') && (this.haveLamp === true) && (this.haveRope === true))) {
           this.game.state.start(ep.destination);
         }
       }
     }, this);
 
+    //Talking to NPCs
     if (spaceKey.isDown && dialogue.hidden) {
       this.npcs.forEach(function(npc) {
         npc.interact();
       },this);
-
     }
-
-
     if (spaceKey.isDown && !dialogue.typing && !dialogue.hidden) {
       dialogue.hide();
     }
@@ -225,9 +226,6 @@ Game.Town.prototype = {
     player.update();
    
   },
-  // render: function() {
-  //   this.jack.body.debug = true;
-  // }
 
 };
 
